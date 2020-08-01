4609533e6a42f86c0a00bc9b3a7e1853d0df045d
Rishabh Shekhar <rishabhshekhar04@gmail.com>
2020-08-01 21:49:47 +0530

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns=[
+    url('relative/',views.relative,name='relative'),
+    url('other/',views.other,name='other'),
+
+]
